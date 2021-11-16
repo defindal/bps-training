59e41abbe8cf7366f19d66e686ec1f4563b8a3e2
defindal <defindal@gmail.com>
2021-11-16 14:29:40 +0700

    init commit training project

---
+++ src/components/Receiver.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>{{header}}</div>
+</template>
+<script>
+import { bus } from '../main';
+export default {
+  name: 'Receiver',
+  props: {
+    msg: String,
+    header: String
+  },
+  created (){
+    bus.$on('channel01', (data) => {
+      this.header = data;
+    })
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
